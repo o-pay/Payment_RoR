12d6eea1e1b3aaeedbbd48536598b90b2e706f88
allen.chang <allen.chang@ecpay.com.tw>
2019-01-17 16:07:55 +0800

    移除電子發票相關參數

--- Example/Ruby/aio_check_out_atm.rb
+++ Example/Ruby/aio_check_out_atm.rb
@@ -21,42 +21,12 @@ class YOURCONTROLLER < ApplicationController
       #'HoldTradeAMT' => '1',
       #'UseRedeem' => 'N'
     }
-
-
-    ## 若要測試開立電子發票，請將inv_params內的"所有"參數取消註解 ##
-    inv_params = {
-=begin      
-      'RelateNumber' => 'PLEASE MODIFY',  #請帶30碼uid ex: SJDFJGH24FJIL97G73653XM0VOMS4K
-      'CustomerID' => 'MEM_0000001',  #會員編號
-      'CustomerIdentifier' => '',   #統一編號
-      'CustomerName' => '測試買家',
-      'CustomerAddr' => '測試用地址',
-      'CustomerPhone' => '0123456789',
-      'CustomerEmail' => 'johndoe@test.com',
-      'ClearanceMark' => '2',
-      'TaxType' => '1',
-      'CarruerType' => '',
-      'CarruerNum' => '',
-      'Donation' => '2',
-      'LoveCode' => '',
-      'Print' => '1',
-      'InvoiceItemName' => '測試商品1|測試商品2',
-      'InvoiceItemCount' => '2|3',
-      'InvoiceItemWord' => '個|包',
-      'InvoiceItemPrice' => '35|10',
-      'InvoiceItemTaxType' => '1|1',
-      'InvoiceRemark' => '測試商品1的說明|測試商品2的說明',
-      'DelayDay' => '0',
-      'InvType' => '07'
-=end      
-    }
-
     pay_info_url = 'http://192.168.0.1'
     exp = '7'
-    cli_redir_url 'http://192.168.0.1/payment_result'
+    cli_redir_url='http://192.168.0.1/payment_result'
 
     create = AllpayPayment::PaymentClient.new
-    htm = create.aio_check_out_atm(params: base_param, url_return_payinfo: pay_info_url, exp_period: exp, client_redirect:cli_redir_url, invoice:inv_params)
+    htm = create.aio_check_out_atm(params: base_param, url_return_payinfo: pay_info_url, exp_period: exp, client_redirect:cli_redir_url)
     render :text => htm
   end
 end
--- Example/Ruby/aio_check_out_credit_divide.rb
+++ Example/Ruby/aio_check_out_credit_divide.rb
@@ -22,40 +22,11 @@ class YOURCONTROLLER < ApplicationController
       #'UseRedeem' => 'N'
     }
 
-
-    ## 若要測試開立電子發票，請將inv_params內的"所有"參數取消註解 ##
-    inv_params = {
-=begin      
-      'RelateNumber' => 'PLEASE MODIFY',  #請帶30碼uid ex: SJDFJGH24FJIL97G73653XM0VOMS4K
-      'CustomerID' => 'MEM_0000001',  #會員編號
-      'CustomerIdentifier' => '',   #統一編號
-      'CustomerName' => '測試買家',
-      'CustomerAddr' => '測試用地址',
-      'CustomerPhone' => '0123456789',
-      'CustomerEmail' => 'johndoe@test.com',
-      'ClearanceMark' => '2',
-      'TaxType' => '1',
-      'CarruerType' => '',
-      'CarruerNum' => '',
-      'Donation' => '2',
-      'LoveCode' => '',
-      'Print' => '1',
-      'InvoiceItemName' => '測試商品1|測試商品2',
-      'InvoiceItemCount' => '2|3',
-      'InvoiceItemWord' => '個|包',
-      'InvoiceItemPrice' => '35|10',
-      'InvoiceItemTaxType' => '1|1',
-      'InvoiceRemark' => '測試商品1的說明|測試商品2的說明',
-      'DelayDay' => '0',
-      'InvType' => '07'
-=end      
-    }
-
-    inst = 12 #分期期數
+    inst = '12' #分期期數
     amt = 100 #分期總金額
 
     create = AllpayPayment::PaymentClient.new
-    htm = create.aio_check_out_credit_divide(params: base_param, invoice: inv_params, installment: inst, amount:amt)
+    htm = create.aio_check_out_credit_divide(params: base_param, installment: inst, amount:amt)
     render :text => htm
   end
 end
